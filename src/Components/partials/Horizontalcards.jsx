20f84228e5755b3f33232249cf06802e87467331
Mananm2004 <119405286+Mananm2004@users.noreply.github.com>
2024-09-10 00:45:59 +0530

    Update Horizontalcards.jsx

--- src/Components/partials/Horizontalcards.jsx
+++ src/Components/partials/Horizontalcards.jsx
@@ -12,9 +12,9 @@ function Horizontalcards({data,func}) {
                   {<img className='w-full h-[55%] object-cover' src={`https://image.tmdb.org/t/p/original/${e.backdrop_path || e.poster_path}`}></img>}
                       <div className='text-white p-3 h-[45%] overflow-y-auto'>
                       <h1 className='text-xl   font-semibold '>{e.title || e.name || e.original_title || e.original_name}</h1>
-                     <p className=' mt-3 mb-3 text-white'>{e.overview.slice(0, 50)}
-                     <Link className="text-zinc-400">...more</Link>
-                     </p>
+                     <p className=' mt-3 mb-3 text-white'>{e.overview.slice(0, 50)}</p>
+                     <p className="text-zinc-400">...more</p>
+                     
                      </div>
                 </Link>)):<h1 className='text-3xl text-white font-black text-center mt-5'>Nothing to show</h1>}
           </div>
